1344ad524db20b08d81e794084c7849e4d9e0665
Ram <r.kulkarni@ufl.edu>
2019-08-25 23:07:07 -0400

    Serialize and Deserialize Binary Tree using Preorder traversal

--- AlgoJava/src/LeetCode/Codec.java
+++ AlgoJava/src/LeetCode/Codec.java
@@ -1,36 +1,13 @@
 package LeetCode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Codec {
 
-    // Encodes a tree to a single string.
-    public String serialize2(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root == null) {
-            return "";
-        }
-        String result = new String();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node == null) {
-                result += "N";
-                q.add(null);
-                q.add(null);
-            }
-            else {
-                result += Integer.toString(node.val);
-                q.add(node.left);
-                q.add(node.right);
-            }
-            result += ",";
-        }
-        return result;
-    }
-    
     public String serialize(TreeNode root) {
         Stack<String> result = new Stack();
         preorder(root, result);
@@ -60,55 +37,25 @@ public class Codec {
         //result.add(")");
     }
     
-    
-
-    int findSpecial(int begin, String data) {
-        for (int i = begin; i < data.length(); ++i) {
-            if (data.charAt(i) == '(' || data.charAt(i) == ',' || data.charAt(i) == ')') {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return deserializeUtil(data, 0);
+        List<String> s = Arrays.asList(data.split(","));
+        int[] index = new int[1];
+        return deserializeUtil(s, index);
     }
 
-    public TreeNode deserializeUtil(String data, int begin) {
-        if (begin >= data.length() || begin < 0) {
+    public TreeNode deserializeUtil(List<String> data, int[] index) {
+        if (data.get(index[0]).compareTo("null") == 0) {
             return null;
         }
-        int curr = findSpecial(begin, data);
-        String snum = data.substring(begin, curr);
-        System.out.println(snum);
-        if (snum.compareTo("null") == 0) {
-            return null;
-        }
-        int num = Integer.parseInt(snum);
-        TreeNode node = new TreeNode(num);
-        if (data.charAt(curr) == '(') {
-            System.out.println("left");
-            node.left = deserializeUtil(data, curr+1);
-        }
-        System.out.println(node.val + "right");
-        begin = curr + 1;
-        curr = findSpecial(begin, data);
-        snum = data.substring(begin, curr);
-        System.out.println(snum);
-        if (snum.compareTo("null") == 0) {
-            node.right = null;
-        }
-        else {
-            num = Integer.parseInt(snum);
-            node.right = deserializeUtil(data, curr+1);
-        }
+        TreeNode node = new TreeNode(Integer.parseInt(data.get(index[0])));
+        index[0] += 1;
+        node.left = deserializeUtil(data, index);
+        index[0] += 1;
+        node.right = deserializeUtil(data, index);
         return node;
     }
 
-    
-    
     public static void main(String[] args) {
       Codec codec = new Codec();
       TreeNode root = new TreeNode(4);
