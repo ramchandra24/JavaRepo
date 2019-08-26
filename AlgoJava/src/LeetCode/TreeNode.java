ad479f1ae25daae6e7f1401a8ea046fb6a79a99a
Ram <r.kulkarni@ufl.edu>
2019-08-25 21:31:20 -0400

    Serialize Deserialize Binary Tree. Debug Deserialize

--- AlgoJava/src/LeetCode/TreeNode.java
+++ AlgoJava/src/LeetCode/TreeNode.java
@@ -5,4 +5,13 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+    
+    public static void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
 }
