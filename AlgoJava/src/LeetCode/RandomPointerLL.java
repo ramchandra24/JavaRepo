0de7a847f170df460cb65206aa754b789c71ae0d
Ram <r.kulkarni@ufl.edu>
2019-09-17 21:54:59 -0400

    LinkedList with random pointer

--- AlgoJava/src/LeetCode/RandomPointerLL.java
+++ AlgoJava/src/LeetCode/RandomPointerLL.java
@@ -19,33 +19,40 @@ public class RandomPointerLL {
         }
     }
 
+    public Node getNode(Map<Node, Node> rmap, Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (rmap.containsKey(node)) {
+            return rmap.get(node);
+        } else {
+            Node n = new Node();
+            n.val = node.val;
+            n.next = n.random = null;
+            rmap.put(node, n);
+            return n;
+        }
+    }
+
     public Node copyRandomList(Node head) {
         Map<Node, Node> rmap = new HashMap<>();
+        if (head == null) {
+            return head;
+        }
         Node nhead = new Node();
         Node ret = nhead;
+        nhead.val = head.val;
+        rmap.put(head, nhead);
+
         while (head != null) {
-            nhead.val = head.val;
-            nhead.random = null;
-            nhead.next = null;
-            if (head.random == head) {
-                nhead.random = nhead;
-            }
-            if (rmap.containsKey(head)) {
-                Node linknode = rmap.get(head);
-                linknode.random = nhead;
-                rmap.remove(head);
-            } else {
-                rmap.put(head.random, nhead);
-            }
+            nhead.random = getNode(rmap, head.random);
+            nhead.next = getNode(rmap, head.next);
             head = head.next;
-            if (head != null) {
-                nhead.next = new Node();
-                nhead = nhead.next;
-            }
+            nhead = nhead.next;
         }
         return ret;
     }
-    
+
     public void printList(Node head) {
         while (head != null) {
             System.out.print(head.val + "->" + (head.random != null ? head.random.val : "") + " :: ");
@@ -53,7 +60,7 @@ public class RandomPointerLL {
         }
         System.out.println();
     }
-    
+
     public static void main(String[] args) {
         RandomPointerLL ll = new RandomPointerLL();
         RandomPointerLL.Node head = ll.new Node();
