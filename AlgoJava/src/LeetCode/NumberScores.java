3f4b3caafe4a7e6423dadb0b7a140c82758cf85c
Ram <r.kulkarni@ufl.edu>
2019-10-10 22:22:49 -0400

    Number scores. Digit operation

---
+++ AlgoJava/src/LeetCode/NumberScores.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class NumberScores {
+
+    static int[] digitArr(int number) {
+        String temp = Integer.toString(number);
+        int[] newGuess = new int[temp.length()];
+        for (int i = 0; i < temp.length(); i++) {
+            newGuess[i] = temp.charAt(i) - '0';
+        }
+        return newGuess;
+    }
+
+    static int compute_number_score(int number) {
+        int[] digits = digitArr(number);
+
+        int sum = 0;
+        sum += digits.length % 5 == 0 ? 6 : 0;
+
+        sum += Arrays.stream(digits).filter(num -> num % 2 != 0).count();
+
+        sum += (Arrays.stream(digits).filter(num -> num == 5).count()) * 2;
+
+        int streak = 1;
+        for (int i = 1; i < digits.length; ++i) {
+            if (digits[i] - digits[i - 1] == 1) {
+                ++streak;
+            } else {
+                sum += streak * streak;
+                streak = 1;
+            }
+        }
+        if (streak > 1) {
+            sum += streak * streak;
+        }
+
+        for (int i = 1; i < digits.length; ++i) {
+            if (digits[i] == 3 && digits[i - 1] == 3) {
+                sum += 4;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compute_number_score(101275345));
+    }
+}
